7e106366091492958eb82996722423e0b523da77
IsDilanZ <dilanismael62@gmail.com>
2024-05-28 22:12:56 -0500

    3rd commit

--- src/main/java/com/example/repo4_orderarray/HelloApplication.java
+++ src/main/java/com/example/repo4_orderarray/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class HelloApplication extends Application {
     @Override
@@ -13,6 +14,26 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+
+        int[] NumbersArray = {100, 10, 2, 0, -1, 4, 3, 1000};
+
+        int lengthArray = NumbersArray.length;
+
+        // Loop to sort array using bubble method
+        for (int i = 0; i < lengthArray - 1; i++) {
+            for (int j = 0; j < lengthArray - 1; j++) {
+                if (NumbersArray[j] > NumbersArray[j + 1]) {
+                    int temp = NumbersArray[j];
+                    NumbersArray[j] = NumbersArray[j + 1];
+                    NumbersArray[j + 1] = temp;
+                }
+            }
+        }
+
+        // print ordered array
+        System.out.println("Ordered array: ");
+        for (int i = 0 ; i<lengthArray ; i++) {
+            System.out.print(NumbersArray[i] + " ");
+        }
     }
 }
